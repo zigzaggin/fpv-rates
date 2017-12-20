480162acb16ea94b3ab9fb89ec37e5853decd02c
benjamin potter <benjamin.potter@live.com>
2017-12-19 20:21:59 -0700

    Roughing in header

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
-
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+app.locals.site = require('./settings');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,8 +29,8 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//urls
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,6 +38,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        home: true,
+        title: 'FPV Rates - All your rates in one place'
+    });
 });
 
 module.exports = router;
