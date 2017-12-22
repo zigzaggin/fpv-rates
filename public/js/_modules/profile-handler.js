fcfefcb5d8efd839770b36a868a78821c64c41bd
Ben Potter <benjamin.potter@live.com>
2017-12-22 10:30:44 -0700

    adding removing working

--- public/js/_modules/profile-handler.js
+++ public/js/_modules/profile-handler.js
@@ -1,26 +1,55 @@
-define(function () {
+define(['jquery'], function ($) {
+    var STORAGE_KEY = 'profiles';
+    var fromStorage = function () {
+        var fromStorage = localStorage.getItem(STORAGE_KEY);
+        if (fromStorage !== null) {
+            return JSON.parse(fromStorage);
+        }
+        return [];
+    };
+    var store = function (profiles) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(profiles));
+    };
+    var addToStorage = function (profileShell) {
+        var profiles = fromStorage();
+        profiles.push(profileShell);
+        store(profiles);
+    };
+    var generateID = function () {
+        var S4 = function () {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        };
+        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+    };
     return {
-        generateID: function () {
-            var S4 = function () {
-                return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-            };
-            return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-        },
         loadProfiles: function () {
-            var profiles;
-            var fromStorage = localStorage.getItem('profiles');
-            if (fromStorage !== null && typeof fromStorage === "object") {
-                profiles = JSON.parse(fromStorage).map(function (value) {
-                    return {id: value.id, description: value.description}
-                });
-            } else {
-                profiles = [{id: this.generateID(), description: "New Profile"}]
+            var profiles = fromStorage();
+            if (profiles.length === 0) {
+                this.generateProfile();
+                profiles = fromStorage();
             }
 
             return profiles;
         },
+        generateProfile: function () {
+            var profileShell = {id: generateID(), description: "New Profile"};
+            addToStorage(profileShell);
+            return profileShell;
+        },
         loadProfile: function (profileId) {
+            return $.grep(fromStorage(), function (e) {
+                return e.id === profileId
+            })[0];
+        },
+        removeProfile: function (profileId) {
+            var profiles = fromStorage();
 
+            var index = profiles.findIndex(function (value) {
+                return value.id === profileId;
+            });
+            profiles.splice(index, 1);
+
+            store(profiles);
         }
     };
 });
--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -9,12 +9,46 @@ requirejs(
         $(function () {
             var page = $("#home-page-target");
 
+            var activateProfile = function (id) {
+                var profile = Handler.loadProfile(id);
+                page.find("[data-content-target]").html(content(profile));
+                page.find("[data-profile]").removeClass("active");
+                page.find("[data-profile='" + id + "']").addClass("active");
+            };
+
             var renderRail = function () {
                 var profiles = Handler.loadProfiles();
                 page.find("[data-rail-target]").html(rail({profiles: profiles}));
+                activateProfile(profiles[0].id);
             };
 
-            renderRail();
+            var addProfile = function () {
+                var generated = Handler.generateProfile();
+                renderRail();
+                activateProfile(generated.id);
+            };
 
+            var removeProfile = function () {
+                var me = $(this);
+                var parent = me.parents("[data-profile]");
+                var id = parent.data("profile");
+                var currentActive = page.find("[data-profile].active").data("profile");
+                Handler.removeProfile(id);
+                parent.remove();
+
+                renderRail();
+
+                if (page.find("[data-profile='" + currentActive + "']").length > 0)
+                    activateProfile(currentActive);
+            };
+
+            page.on("click", "[data-add-profile]", addProfile);
+            page.on("click", "[data-profile] > span", function (e) {
+                var profile = $(this).parents("[data-profile]");
+                activateProfile(profile.data("profile"));
+            });
+            page.on("click", "[data-remove-profile]", removeProfile);
+
+            renderRail();
         });
     });
